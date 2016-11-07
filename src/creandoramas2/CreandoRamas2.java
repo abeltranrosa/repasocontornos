be3d8a897d31b1aefd1ff0ba1db2be5a3ae74129
antonio <antonio@biruseto>
2016-11-07 09:26:09 +0100

    inicio proyecto

---
+++ src/creandoramas2/CreandoRamas2.java
@@ -0,0 +1,21 @@
+
+package creandoramas2;
+
+import javax.swing.JOptionPane;
+
+
+public class CreandoRamas2 {
+
+   
+    public static void main(String[] args) {
+        float base;
+        float altura;
+        float area;
+        
+        base=Float.parseFloat(JOptionPane.showInputDialog("base"));
+        altura=Float.parseFloat(JOptionPane.showInputDialog("altura"));
+        area=base*altura/2;
+        System.out.println("area es:"+(base*altura));
+    }
+    
+}
